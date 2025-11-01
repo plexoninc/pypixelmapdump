d8b970157eed587417e19714d83933444c656c70
Nikhil Chandra <nikhil@plexon.com>
2025-10-31 21:19:32 -0500

    fix: Added sys._MEIPASS so pixelmapdump.exe works nicely with PyInstaller.

--- src/pypixelmapdump/pixelmapdump.py
+++ src/pypixelmapdump/pixelmapdump.py
@@ -1,13 +1,27 @@
+import os, sys
 import re
 import subprocess
 from pathlib import Path
 import tempfile
 
 def get_pixel_map_dump(pl2_file_path):
+    # Determine where to look for the pixelmapdump.exe file
+    if getattr(sys, "frozen", False):
+        parent_folder = Path(sys._MEIPASS)
+    else: 
+        parent_folder = Path(__file__).parent
+    
+    executable_path = parent_folder / "pixelmapdump.exe"
+    if not executable_path.exists():
+        raise FileNotFoundError(f"Pixelmapdump executable not found at {executable_path}")
+
     with tempfile.NamedTemporaryFile(mode="w+") as tmp_out:
         with tempfile.NamedTemporaryFile(mode="w+") as tmp_err:
-            parent_folder = Path(__file__).parent
-            executable_path = str(parent_folder / "pixelmapdump.exe")
+            print("sys.frozen:", getattr(sys, "frozen", False))
+            print("sys._MEIPASS:", getattr(sys, "_MEIPASS", None))
+            print("executable_path:", executable_path)
+            print("Exists?", os.path.exists(executable_path))
+
             p = subprocess.Popen([executable_path, pl2_file_path], stdout=tmp_out, stderr=tmp_err, text=True)
             p.wait()
 
